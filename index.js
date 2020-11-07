6b245cedb1facb9fe82a8487dbf98eabf8cb4d63
bph <birgit.pauli@gmail.com>
2020-11-07 17:12:59 -0500

    First version

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+console.log(`
+
+Pauli Systems
+Services: DevOps, Web devs, Data Integration, APIs.
+Founded: 2002
+Founder: Birgit Pauli-Haack
+
+Twitter: https://twitter.com/paulisystems
+Website: https://paulisystems.net
+
+`)
