f69d07875c2c6612b2dcfdc3834f5965e7e88dd5
bph <birgit.pauli@gmail.com>
2020-11-08 17:19:05 -0500

    Updates: clear console

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 #!/usr/bin/env node
+const clearConsole = require('clear-any-console');
+// Clears the console.
+clearConsole();
+
 console.log(`
 
-Pauli Systems
+Pauli Systems, LC
 Services: DevOps, Web devs, Data Integration, APIs.
 Founded: 2002
 Founder: Birgit Pauli-Haack
 
 Twitter: https://twitter.com/paulisystems
 Website: https://paulisystems.net
+Email: support@paulisystems.net
 
 `)
