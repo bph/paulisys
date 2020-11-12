75f395a26f5da29315341114fe3ce1adddc24d44
bph <birgit.pauli@gmail.com>
2020-11-11 22:50:26 -0500

    more colors with chalk

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 const welcome = require('cli-welcome');
 const pkgJSON = require('./package.json');
+const chalk = require('chalk');
+const log = console.log; 
 
 welcome ({
-    title: pkgJSON.name,
+    title: `Pauli Systems`,
     tagLine: `NodeJS SysOps`,
     version: pkgJSON.version,
     description: pkgJSON.description,
@@ -13,14 +15,15 @@ welcome ({
     clear: true,
 });
 
-console.log(`
-Pauli Systems, LC
-Services: DevOps, SysOps, Web devs, Data Integration, APIs.
+log(`
+
+${chalk.dim(`Services: DevOps, SysOps, Web devs, Data Integration, APIs.
 Founded: 2002
 Founder: Birgit Pauli-Haack
+`)}
 
-Twitter: https://twitter.com/paulisystems
+${chalk.blueBright(`Twitter`)}: https://twitter.com/paulisystems
 Website: https://paulisystems.net
-Email: support@paulisystems.net
+Email:   support@paulisystems.net
 
 `)
