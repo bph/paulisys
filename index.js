47d3ae8b77bf7933006290d8811f1ddd7b98b412
bph <birgit.pauli@gmail.com>
2020-11-17 16:45:09 -0500

    last change from a few days ago

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const pkgJSON = require('./package.json');
 const chalk = require('chalk');
 const log = console.log; 
 
+ // Alerts
+ const success = chalk.green.inverse;
+ const info = chalk.blue.inverse;
+ const warning = chalk.keyword(`orange`).inverse;
+ const error = chalk.red.bold.inverse;
+
 welcome ({
     title: `Pauli Systems`,
     tagLine: `NodeJS SysOps`,
@@ -26,4 +32,11 @@ ${chalk.blueBright(`Twitter`)}: https://twitter.com/paulisystems
 Website: https://paulisystems.net
 Email:   support@paulisystems.net
 
+`)
+
+log(`
+${success(` Success: `)} Thank for checking out the cli
+${info(` Info: `)}    Ahmad Awais is createing a course on NodeCLI.com
+${warning(` Warning: `)} Please don't copy me. Be yourself.
+${error(` Error :`)}   I am on vacation. Contact me next week. 
 `)
