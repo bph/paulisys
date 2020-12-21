f6732e84a72e18ae05620bc0246d6646e48ef67d
bph <birgit.pauli@gmail.com>
2020-12-21 16:49:57 -0500

    added loading spinner

--- utils/stats.js
+++ utils/stats.js
@@ -1,36 +1,22 @@
 // const fetch = require('node-fetch');
 const axios = require('axios');
+const ora = require('ora');
+const { green, yellow} = require('chalk');
 const handleError = require('cli-handle-error');
 const to = require('await-to-js').default;
 const apiUrl = `https://api.github.com/users/bph`;
+const spinner = ora( { text: '' } )
 
 module.exports = async() => {
-   
+    spinner.start(`${yellow(`Followers`)} fetching...`);
     const [error,res] = await to(axios.get(apiUrl));
     handleError(`API call to GitHub failed`,error, false,false);
     
     const ghFollowers = res.data.followers;
+    spinner.succeed(`${green(`Followers`)} fetched. `);
+
     console.log(`
    GitHub followers: ${ghFollowers}
            `);
-
-
-  /* try {
-        const res = await axios.get(apiUrl);
-        const ghFollowers = res.data.followers;
-        console.log(`
-GitHub followers: ${ghFollowers}
-            `);
-   } catch(error) { 
-       handleError(`API call to GitHub failed`,error, false,false);
-   }
-    // console.log('res: ', res);
-   */
-
-    /*
-    const res = await fetch(apiUrl);
-    const data = await res.json();
-    //console.log('data: ', data);
-    */
  }
 
