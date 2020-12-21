b4b5fbd7075b63077869462e6ec071e1a77cad8d
bph <birgit.pauli@gmail.com>
2020-12-21 12:20:02 -0500

    added await-to-js package + code

--- utils/statsaxios.js
+++ utils/statsaxios.js
@@ -1,21 +1,15 @@
 // const fetch = require('node-fetch');
 const axios = require('axios');
 const handleError = require('cli-handle-error');
-const to = require('await-to-js').default;
 const apiUrl = `https://api.github.com/users/bph`;
 
 module.exports = async() => {
-   
-    const [error,res] = await to(axios.get(apiUrl));
-    handleError(`API call to GitHub failed`,error, false,false);
-    
-    const ghFollowers = res.data.followers;
-    console.log(`
-   GitHub followers: ${ghFollowers}
-           `);
-
-
-  /* try {
+    /*
+    const res = await fetch(apiUrl);
+    const data = await res.json();
+    //console.log('data: ', data);
+    */
+   try {
         const res = await axios.get(apiUrl);
         const ghFollowers = res.data.followers;
         console.log(`
@@ -25,12 +19,6 @@ GitHub followers: ${ghFollowers}
        handleError(`API call to GitHub failed`,error, false,false);
    }
     // console.log('res: ', res);
-   */
-
-    /*
-    const res = await fetch(apiUrl);
-    const data = await res.json();
-    //console.log('data: ', data);
-    */
+   
  }
 
