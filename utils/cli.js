053a67a37f9e4643755e40fd5e9203743755e9b3
bph <birgit.pauli@gmail.com>
2020-12-24 17:09:55 -0500

    add cli-meow-help for automated help text

--- utils/cli.js
+++ utils/cli.js
@@ -1,8 +1,10 @@
 const meow = require('meow');
+const meowHelp = require('cli-meow-help');
 const { green, yellow, cyan, dim} = require('chalk');
 
 // const cli = meow(`RUN: npx paulisys`);
 
+/* hand written helptext 
 const helpText = `
     Usage
        ${green(`npx paulisys`)} ${yellow(`[--option]`)} ${cyan(`<command>`)}
@@ -29,46 +31,96 @@ const helpText = `
        ${green(`npx paulisys`)} ${yellow(`--no-ad`)}
 
 `;
-const options = {
-    flags: {
-        minimal: {
-            type: 'boolean',
-            alias: 'm',
-            default: false,
-        },
-        clear: {
-            type: 'boolean',
-            default: true,
-        },
-        social: {
-            type: 'boolean',
-            default: true,
-        },
-        bio: {
-            type: 'boolean',
-            default: true,
-        },
-        ad: {
-            type: 'boolean',
-            default: true,
-        },
-        debug: {
-            type: 'boolean',
-            default: false,
-            alias: 'd',
-        },
-        version: {
-            type: 'boolean',
-            default: false,
-            alias: 'v',
-        },
-        posts: {
-            type: 'boolean',
-            default: false,
-            alias: 'p',
-        }
+*/
+// all will be replaced by the meowHelper call
 
+const flags = {
+    ad: {
+        type: 'boolean',
+        default: true,
+        desc: `Print the ad info`,
     },
+    'no-ad':{
+        type: 'boolean',
+        default: false,
+        desc: `Does not print ad`,
+    },
+    bio: {
+        type: 'boolean',
+        default: true,
+        desc: `Print the bio info`,
+    },
+    'no-bio':{
+        type: 'boolean',
+        default: false,
+        desc: `Does not print bio info`,
+    },
+    clear: {
+        type: 'boolean',
+        default: true,
+        desc: `Clear the console`,
+    },
+    'no-clear':{
+        type: 'boolean',
+        default: false,
+        desc: `Does not clear the console.`,
+    },
+    debug: {
+        type: 'boolean',
+        default: false,
+        alias: 'd',
+    },
+    minimal: {
+        type: 'boolean',
+        alias: 'm',
+        default: false,
+        desc: `Print minimal info`,
+    },
+    posts: {
+        type: 'boolean',
+        default: false,
+        alias: 'p',
+        desc: `Show list of 10 blog posts`,
+    },
+    'no-posts': {
+        type: 'boolean',
+        default: false,
+        desc: `Don't show blog posts`,
+    },
+    social: {
+        type: 'boolean',
+        default: true,
+        desc: `Print the social info`,
+    },
+    'no-social': {
+        type: 'boolean',
+        default: false,
+        desc: `Does not print ad`,
+    },
+    version: {
+        type: 'boolean',
+        default: false,
+        alias: 'v',
+        desc: `Print CLI version info`,
+    },
+};
+const commands = {
+    help: {
+        desc: `Print the help info`,
+    },
+};
+
+const helpText = meowHelp({
+    name: `npx paulisys`,
+    flags,
+    commands,
+    defaults: false,
+})
+const options = {
+    inferType: true,
+    description: false,
+    hardRejections: false,
+    flags,
 };
 
 module.exports = meow(helpText, options);
